6f0ebe86b5512fa0581203858038a988d710150a
Tenjo16 <raydin2001@gmail.com>
2026-06-20 00:30:05 -0700

    New Project

---
+++ gens.js
@@ -0,0 +1,16 @@
+let text = document.querySelector('.text-box');
+let btn = document.querySelector('.button');
+let img = document.querySelector('.gen-image');
+
+btn.addEventListener('click', function () {
+    if (text.value >= 1900 && text.value <= 1998) {
+        img.classList.remove('hidden');
+        img.src = "./images/ermagherd.jpg";
+    } else if (text.value < 1900) {
+        img.classList.remove('hidden');
+        img.src = "./images/grave.jpg";
+    } else if (text.value >= 2010 && text.value <= 2018) {
+        img.classList.remove('hidden');
+        img.src = "./images/doge.jpg";
+    }
+});
