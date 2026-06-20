762b26609f12b836cb36ac1834aacb5d1a502c56
Tenjo16 <raydin2001@gmail.com>
2026-06-20 00:46:47 -0700

    Range fix

--- gens.js
+++ gens.js
@@ -12,7 +12,7 @@ btn.addEventListener('click', function () {
     } else if (text.value >= 2000 && text.value <= 2009) {
         img.classList.remove('hidden');
         img.src = "./images/doge.jpg";
-    } else if (text.value >= 2010 && text.value <= 2018) {
+    } else if (text.value >= 2018) {
         img.classList.remove('hidden');
         img.src = "./images/car.jfif";
     }
